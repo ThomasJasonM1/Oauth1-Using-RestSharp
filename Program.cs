7320ac74f7b576bbe8ad591cdfa90d073dc3e447
Jason Thomas <jason@call-em-all.com>
2021-07-16 15:55:41 -0500

    Adds additional comments

--- Program.cs
+++ Program.cs
@@ -7,12 +7,17 @@ namespace Oauth1_Test
     class Program
     {
         // Fill in these 4 variables
+        // The consumerKey, consumerSecret and tokenValue can be found in the welcome email your received when you gained access to use our API
+        // The conversationId can come from the excel file you received earlier today
         private static string conversationId = "";
         private static string consumerKey = "";
         private static string consumerSecret = "";
         private static string tokenValue = "";
+
+
         // You don't need to modify anything below this line
         private static string tokenSecret = ""; // Leave this an empty string
+        // This URL can be modified based on the endpoint you want to call
         private static string baseUrl = "https://staging-rest.call-em-all.com/v1/{path}";
 
         static void Main(string[] args)
